efb458932966c041182cc6e833c48cf764454799
abdureufshifa <shifabdureuf@gmail.com>
2023-08-24 23:32:10 +0300

    python import modules

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def raise_exception():
-    raise TypeError()
+    raise TypeError("Exception raised")
 
 try:
     raise_exception()
 except TypeError as te:
-    print("Exception raised")
+    print(te)
