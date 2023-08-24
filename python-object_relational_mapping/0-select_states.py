124f771b0c0cf6060536c269e0b4189d37c8d9da
abdureufshifa <shifabdureuf@gmail.com>
2023-08-24 10:52:09 +0300

    first check

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,44 @@
+"""
+import system and mysqldb
+"""
+import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    """
+    create methods that pass 3 arguments.
+    """
+    # Connect to the MySQL server
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # Execute the query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    # Get the MySQL username, password, and database name from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to list the states
+    list_states(username, password, database)
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+# python-object_relational_mapping
+This project is about python _ORM
