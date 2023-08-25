db58766f72a25756920c3658a1480bf5769b0b7b
abdureufshifa <shifabdureuf@gmail.com>
2023-08-25 15:42:40 +0300

    python-hello_world

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,7 @@
 for i in range (10):
-  for j in range(i+1,9):
+  for j in range(i+1,10):
     if(i !=j or str(i)+ str(j) !=str(j)+str(i)):
-     print("{}, ".format(str(i)+ str(j)),end="")
-print("{}, ".format(79),end="")
+     if(str(i)+ str(j) != '89' ):
+       print("{}, ".format(str(i)+ str(j)),end="")
+# print("{}, ".format(79),end="")
 print(89)  
