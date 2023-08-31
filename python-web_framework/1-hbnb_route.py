ba907e7b76b323d1533dd77d9c6a91a022cb98e7
abdureufshifa <shifabdureuf@gmail.com>
2023-08-31 23:27:49 +0300

    python-web

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -12,7 +12,13 @@ def hello():
     create methods that return Hello HBNB!
     """
     return 'Hello HBNB!'
-    
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    create methods that return  HBNB!
+    """
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -12,7 +12,20 @@ def hello():
     create methods that return Hello HBNB!
     """
     return 'Hello HBNB!'
-    
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    create methods that return  HBNB!
+    """
+    return 'HBNB'
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    create methods that return  C with text
+    """
+   
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
