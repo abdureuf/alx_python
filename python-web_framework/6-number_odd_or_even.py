1289f5af02ef573e6691d8556566aec6cdb3881f
abdureufshifa <shifabdureuf@gmail.com>
2023-09-01 00:09:11 +0300

    python-web

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -49,6 +49,13 @@ def number_template(n):
     if isinstance(n, int):
         return render_template('number.html', number=n)
     
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if isinstance(n, int):
+        """
+        by using render_templete
+        """
+        return render_template('odd_or_even.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
