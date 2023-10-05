e39dee9a4dc5b7b575f886dc5d6bb792acc2f5d8
abdureufshifa <shifabdureuf@gmail.com>
2023-10-05 12:10:19 +0300

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,7 +18,7 @@ def get_employee_todo_progress(employee_id):
     num_done_tasks = len(done_tasks)
 
     # Print progress information
-    print(f"Employee Name: {employee_name}\nTo Do Count: {total_tasks}\n")
+    print(f"Employee Name: {employee_name.ljust(18)}\nTo Do Count: {total_tasks}\n")
 
     # Print completed tasks
     print(f"Task 1 in output: {done_tasks[0]['title']}")
@@ -26,7 +26,7 @@ def get_employee_todo_progress(employee_id):
         print(f"Task {i+1} in output: {done_tasks[i]['title']}")
 
     # Print formatting information
-    print(f"\nThe first line formatting: {'OK' if len(f'Employee Name: {employee_name}') == 26 else 'NOT OK'}")
+    print(f"\nThe first line formatting: {'OK' if len(f'Employee Name: {employee_name.ljust(18)}') == 26 else 'NOT OK'}")
     print(f"The task 1 formatting: {'OK' if len(f'Task 1 in output: {done_tasks[0]['title']}') == 26 else 'NOT OK'}")
     for i in range(1, num_done_tasks):
         task_num = i + 1
