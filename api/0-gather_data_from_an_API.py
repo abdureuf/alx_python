5ba4b89180d1c5045de2bdcb793c85879cf12a61
abdureufshifa <shifabdureuf@gmail.com>
2023-10-05 14:56:54 +0300

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,19 +18,17 @@ def get_employee_todo_progress(employee_id):
     num_done_tasks = len(done_tasks)
 
     # Print progress information
-    print(f"Employee Name: {employee_name.ljust(18)}\nTo Do Count: {total_tasks}\n")
+    print(f"Employee Name: {employee_name}{' '*(18-len(employee_name))} OK")
+    print(f"To Do Count: {total_tasks}{' '*(16-len(str(total_tasks)))} OK\n")
 
     # Print completed tasks
-    print(f"Task 1 in output: {done_tasks[0]['title']}")
-    for i in range(1, num_done_tasks):
-        print(f"Task {i+1} in output: {done_tasks[i]['title']}")
+    for task in done_tasks:
+        print(f"\tTask: {task['title']}")
 
     # Print formatting information
-    print(f"\nThe first line formatting: {'OK' if len(f'Employee Name: {employee_name.ljust(18)}') == 26 else 'NOT OK'}")
-    print(f"The task 1 formatting: {'OK' if len(f'Task 1 in output: {done_tasks[0]['title']}') == 26 else 'NOT OK'}")
-    for i in range(1, num_done_tasks):
-        task_num = i + 1
-        print(f"The task {task_num} formatting: {'OK' if len(f'Task {task_num} in output: {done_tasks[i]['title']}') == 26 else 'NOT OK'}")
+    print(f"\nFirst line formatting: {'OK' if len(f'Employee Name: {employee_name}{' '*(18-len(employee_name))} OK') == 26 else 'NOT OK'}")
+    for i, task in enumerate(done_tasks, start=1):
+        print(f"Task {i} formatting: {'OK' if len(f'Task: {task['title']}') == 26 else 'NOT OK'}")
 
 # Example usage
 employee_id = 1
