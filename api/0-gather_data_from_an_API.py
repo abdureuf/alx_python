6182fcccb6e1ce3343154b78cbbefab3b2604589
abdureufshifa <shifabdureuf@gmail.com>
2023-10-05 11:56:55 +0300

    api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_employee_todo_progress(employee_id):
+    # Get employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(employee_url)
+    employee_data = response.json()
+    employee_name = employee_data['name']
+
+    # Get employee's TODO list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(todos_url)
+    todos_data = response.json()
+
+    # Calculate progress
+    total_tasks = len(todos_data)
+    done_tasks = [todo for todo in todos_data if todo['completed']]
+    num_done_tasks = len(done_tasks)
+
+    # Print progress information
+    print(f"Employee {employee_name} is done with tasks ({num_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t{task['title']}")
+
+# Example usage
+employee_id = 1
+get_employee_todo_progress(employee_id)
---
+++ api/README.md
@@ -0,0 +1,1 @@
+this api assignment
