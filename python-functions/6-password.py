209fdf0365413f2abc80d865eff0b8d530854aea
abdureufshifa <shifabdureuf@gmail.com>
2023-10-15 02:59:48 +0300

    python-functions

---
+++ python-functions/6-password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def validate_password(password):
+    if len(password) < 8:
+        return False
+
+    if not any(char.isupper() for char in password):
+        return False
+
+    if not any(char.islower() for char in password):
+        return False
+
+    if not any(char.isdigit() for char in password):
+        return False
+
+    if ' ' in password:
+        return False
+
+    return True
