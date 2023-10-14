8f19849fcfaa16bbfbdb9e043f24a8bb84e1c2a6
abdureufshifa <shifabdureuf@gmail.com>
2023-10-15 01:58:59 +0300

    python hello world

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-number2 = str(number)
-last_number = int(number2[-1])
+number_str = str(number)
+last_digit_str = number_str[-1]
+last_digit = int(last_digit_str)
 
-if (number >= 0):
-    if (last_number > 5):
-        print("Last digit of {} is {} and is greater than 5".format(
-            number2, last_number))
-    elif (last_number < 6 and last_number != 0):
-        print("Last digit of {} is {} and is less than 6 and not 0".format(
-            number2, last_number))
+if number >= 0:
+    if last_digit > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number_str, last_digit))
+    elif last_digit < 6 and last_digit != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number_str, last_digit))
     else:
-        print("Last digit of {} is {} and is 0".format(number2, last_number))
+        print("Last digit of {} is {} and is 0".format(number_str, last_digit))
 else:
-
-    if (last_number == 0):
-        print("Last digit of {} is {} and is 0".format(number2, last_number))
+    if last_digit == 0:
+        print("Last digit of {} is {} and is 0".format(number_str, last_digit))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(
-            number2, -last_number))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number_str, -last_digit))
