76f22db1f3746313a2150f8ab1d6ed54490602d9
abdureufshifa <shifabdureuf@gmail.com>
2023-10-15 03:22:36 +0300

    python-import_modules

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-def raise_exception_msg(message=""):
-    raise NameError(message)
-
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+def raise_exception_msg(message):
+    try:
+        raise NameError(message)
+    except NameError as ne:
+        return ne
