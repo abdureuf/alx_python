7973866778a636daefec9a0b7223a216c5b05243
abdureufshifa <shifabdureuf@gmail.com>
2023-10-16 16:53:49 +0300

    python-inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,10 +4,18 @@ no import
 """
 class BaseGeometry:
     """
-    BaseGeometry is an empty class.
+    BaseGeometry is a class with an area method that raises an exception.
     """
     def __dir__(self):
         """
         Customizes the behavior of dir() method.
         """
-    pass
+        attrs = set(dir(type(self))) | set(self.__dict__) | set(dir(BaseGeometry))
+        attrs = attrs - {'__init_subclass__'}  # Exclude '__init_subclass__' method
+        return sorted(attrs)
+
+    def area(self):
+        """
+        Raises an exception when called.
+        """
+        raise Exception("area() is not implemented")
