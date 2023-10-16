2c9361872caf76b0e91960b23eae9d89badae7d1
abdureufshifa <shifabdureuf@gmail.com>
2023-10-16 11:09:39 +0300

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 class Square:
+    """
+    Square class represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
