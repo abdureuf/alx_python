17c960e15ef7b0b0feeb62b26ab3d93dc974f16f
abdureufshifa <shifabdureuf@gmail.com>
2023-10-16 11:19:24 +0300

    python-classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+no import
+"""
+class Square:
+    """
+    Square class represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square (default is 0).
+                Raises:
+                    TypeError: If size is not an integer.
+                    ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
