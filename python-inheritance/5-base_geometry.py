8dab0344aed38c42ed8d1979a5c82274fe00a46f
abdureufshifa <shifabdureuf@gmail.com>
2023-10-16 17:05:30 +0300

    python-inheritance

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,7 +6,14 @@ class BaseGeometry:
     """
     BaseGeometry is a class with area and integer_validator methods.
     """
-    
+    def __dir__(self):
+        """
+        Customizes the behavior of dir() method.
+        """
+        attrs = set(dir(type(self))) | set(self.__dict__) | set(dir(BaseGeometry))
+        attrs = attrs - {'__init_subclass__'}  # Exclude '__init_subclass__' method
+        return sorted(attrs)
+
     def area(self):
         """
         Raises an exception when called.
