b888f11940efb23b04ce7200100157ab0dcfe1ce
abdureufshifa <shifabdureuf@gmail.com>
2023-10-16 16:41:45 +0300

    python-inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,12 +6,6 @@ class BaseGeometry:
     """
     BaseGeometry is an empty class.
     """
-    @classmethod
     def __init_subclass__(cls):
-        pass
-
-    def __dir__(self):
-        return sorted(set(dir(type(self))) | set(self.__dict__) | set(dir(BaseGeometry)))
-
-    def __repr__(self):
-        return "<{} object>".format(self.__class__.__name__)
+        raise NotImplementedError
+    pass
