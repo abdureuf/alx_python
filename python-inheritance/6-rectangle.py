6a4cfd0e6d97843583fef7db57c2faf1d9006f93
abdureufshifa <shifabdureuf@gmail.com>
2023-10-17 10:24:14 +0300

    python-inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-
-import importlib
-
-BaseGeometry = importlib.import_module('5-base_geometry').BaseGeometry
 """
 This module defines the Rectangle class.
 """
 
-# class Rectangle(BaseGeometry):
-#     def __init__(self, width, height):
-#         self.__width = self.integer_validator("width", width)
-#         self.__height = self.integer_validator("height", height)
+import importlib
+
+BaseGeometry = importlib.import_module('5-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle is a class that inherits from BaseGeometry.
-    """
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with width and height.
-        """
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
 
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle instance.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+# class Rectangle(BaseGeometry):
+#     """
+#     Rectangle is a class that inherits from BaseGeometry.
+#     """
+#     def __init__(self, width, height):
+#         """
+#         Initializes a Rectangle instance with width and height.
+#         """
+#         self.__width = self.integer_validator("width", width)
+#         self.__height = self.integer_validator("height", height)
+
+#     def __str__(self):
+#         """
+#         Returns a string representation of the Rectangle instance.
+#         """
+#         return "[Rectangle] {}/{}".format(self.__width, self.__height)
     
