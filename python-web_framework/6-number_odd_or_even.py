538d71ab2108da336d5d27f2cf81850eb63c2350
abdureufshifa <shifabdureuf@gmail.com>
2023-10-17 17:04:08 +0300

    python-web_framework

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,61 +1,43 @@
-#user/bin/python3
-"""
-import flask web framework
-"""
 from flask import Flask, render_template
-# instantiate flask
+
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    """
-    create methods that return Hello HBNB!
-    """
+def hello_hbnb():
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    create methods that return  HBNB!
-    """
     return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """
-    create methods that return  C with text
-    """
-   
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """
-    create methods that return  by default IS COOL
-    """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """
-    create methods that return  only integeres
-    """
     return '{} is a number'.format(n)
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """
-    by using render_templete
-    """
-    if isinstance(n, int):
-        return render_template('number.html', number=n)
-    
+    return render_template('5-number.html', number=n)
+
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    if isinstance(n, int):
-        """
-        by using render_templete
-        """
-        return render_template('odd_or_even.html', number=n)
+    return render_template('6-number_odd_or_even.html', number=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
