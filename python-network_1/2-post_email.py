fc5d158dc00c15cf96183431df94c52cedb1e065
abdureufshifa <shifabdureuf@gmail.com>
2023-10-17 16:22:46 +0300

    python-network_1

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+"""
+comments here
+"""
+import requests
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+payload = {'email': email}
+response = requests.post(url, data=payload)
+
+print("Your email is:", email)
+print(response.text)
